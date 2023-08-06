54dc9d51c724e09bf2940a38709605da95eabdc7
Matthieu Mota <matthieumota@gmail.com>
2023-08-06 10:50:33 +0200

    First components

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const totalMovies = ref(Math.floor(Math.random() * 35000))
+</script>
+
+<template>
+  <footer>
+    <p>
+      Copyright &copy; {{ new Date().getFullYear() }}. Catalogue de
+      <strong>{{ totalMovies }}</strong> films.
+    </p>
+  </footer>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+
+const logged = ref(false)
 </script>
 
 <template>
@@ -7,6 +10,12 @@ import { RouterLink } from 'vue-router'
     <nav>
       <RouterLink to="/">Accueil</RouterLink>
       <RouterLink to="/a-propos">A propos</RouterLink>
+      <template v-if="logged">
+        <span>Fiorella</span>
+        <img src="https://i.pravatar.cc/50?u=fiorella@boxydev.com" alt="Fiorella" />
+        <button @click="logged = false">X</button>
+      </template>
+      <button v-else @click="logged = true">Connexion</button>
     </nav>
   </header>
 </template>
