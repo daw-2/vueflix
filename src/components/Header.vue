ce595b99b19cedf4c4fb751c33b94898893adcb1
Matthieu Mota <matthieumota@gmail.com>
2023-08-06 10:40:33 +0200

    Init

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Accueil</RouterLink>
+      <RouterLink to="/a-propos">A propos</RouterLink>
+    </nav>
+  </header>
+</template>
