85c851210f3ab82f957bd5385152e7f2b5e8969f
Matthieu Mota <matthieumota@gmail.com>
2023-08-07 09:15:42 +0200

    Improve api dump

--- api/README.md
+++ api/README.md
@@ -15,3 +15,9 @@ Il faut mettre votre clé TMDB dans la variable `MOVIE_DB_API_KEY`.
 Et voilà, vous avez l'API sur `http://localhost:3000`.
 
 J'utilise la base de [Json Server](https://github.com/typicode/json-server), sur la documentation tout est indiqué pour réaliser les requêtes en GET, POST, PUT ou DELETE. J'ai aussi ajouté la partie authentification avec ce [middleware](https://github.com/jeremyben/json-server-auth). On peut donc s'inscrire ou se connecter sur cette API.
+
+Voici les URLs de l'API :
+
+- http://localhost:3000/movies => Tous les films
+- http://localhost:3000/genres => Toutes les catégories
+- http://localhost:3000/actors => Tous les acteurs
--- api/dump.js
+++ api/dump.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const { $fetch } = require('ofetch')
+const slugify = require('slugify')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -8,48 +9,57 @@ const baseUrl = 'https://api.themoviedb.org/3'
 const getGenres = async () => {
   const { genres } = await $fetch(`${baseUrl}/genre/movie/list?api_key=${process.env.MOVIE_DB_API_KEY}&language=fr-FR`)
 
-  return genres
+  return genres.map(genre => ({
+    ...genre,
+    slug: slugify(genre.name, { lower: true }),
+  }))
 }
 
-const getMoviesOrActors = async () => {
-  const { results } = await $fetch(`${baseUrl}/movie/now_playing?api_key=${process.env.MOVIE_DB_API_KEY}&language=fr-FR`)
+const getMoviesOrActors = async (pages = 2) => {
   let actors = []
+  let movies = []
 
-  const movies = await Promise.all(results.map(async (movie) => {
-    const result = await $fetch(`${baseUrl}/movie/${movie.id}?api_key=${process.env.MOVIE_DB_API_KEY}&language=fr-FR&append_to_response=videos,credits`)
-    const { genres, videos } = result
-    const actorsMovie = result.credits.cast.slice(0, 5) // [{}, {}]
+  for (let page = 1; page <= pages; page++) {
+    const { results } = await $fetch(`${baseUrl}/movie/now_playing?api_key=${process.env.MOVIE_DB_API_KEY}&language=fr-FR&page=${page}`)
 
-    actorsMovie.forEach((actor) => {
-      let existing = actors.find(a => a.id === actor.id)
+    movies = movies.concat(await Promise.all(results.map(async (movie) => {
+      const result = await $fetch(`${baseUrl}/movie/${movie.id}?api_key=${process.env.MOVIE_DB_API_KEY}&language=fr-FR&append_to_response=videos,credits`)
+      const { genres, videos } = result
+      const actorsMovie = result.credits.cast.slice(0, 5) // [{}, {}]
 
-      if (existing) {
-        existing.moviesId.push(movie.id)
-      } else {
-        actors.push({
-          ...actor,
-          avatar: `https://image.tmdb.org/t/p/w500${actor.profile_path}`,
-          moviesId: [movie.id]
-        })
+      actorsMovie.forEach((actor) => {
+        let existing = actors.find(a => a.id === actor.id)
+
+        if (existing) {
+          existing.moviesId.push(movie.id)
+        } else {
+          actors.push({
+            ...actor,
+            profile_path: `https://image.tmdb.org/t/p/w500${actor.profile_path}`,
+            slug: slugify(actor.name, { lower: true }),
+            moviesId: [movie.id]
+          })
+        }
+      })
+
+      return {
+        ...(delete result.genres && delete result.credits && delete result.videos && delete result.spoken_languages
+          && delete result.belongs_to_collection && delete result.production_companies && delete result.production_countries && result),
+        backdrop_path: `https://image.tmdb.org/t/p/original${result.backdrop_path}`,
+        poster_path: `https://image.tmdb.org/t/p/w500${result.poster_path}`,
+        slug: slugify(result.title, { lower: true }),
+        youtube: videos.results[0]?.key,
+        genreId: genres[0].id,
+        actorId: actorsMovie.map(actor => actor.id),
       }
-    })
-
-    return {
-      ...(delete result.genres && delete result.credits && delete result.videos && delete result.spoken_languages
-        && delete result.belongs_to_collection && delete result.production_companies && delete result.production_countries && result),
-      backdrop_path: `https://image.tmdb.org/t/p/original${result.backdrop_path}`,
-      poster_path: `https://image.tmdb.org/t/p/w500${result.poster_path}`,
-      youtube: videos.results[0]?.key,
-      genreId: genres[0].id,
-      actorId: actorsMovie.map(actor => actor.id),
-    }
-  }))
+    })))
+  }
 
   return { actors, movies }
 }
 
 (async () => {
-  const { movies, actors } = await getMoviesOrActors()
+  const { movies, actors } = await getMoviesOrActors(2)
 
   const data = {
     genres: await getGenres(),
